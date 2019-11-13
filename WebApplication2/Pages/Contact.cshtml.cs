10d293dbd5ac1c7039cd275c8b7f9719d9524f79
eempc <evc484@gmail.com>
2019-11-13 15:14:37 +0000

    git ignore test and starting contact

---
+++ WebApplication2/Pages/Contact.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
+using System.Net.Mail;
+
+namespace WebApplication2.Pages {
+    public class ContactModel : PageModel {
+        [BindProperty]
+        public ContactFormModel Contact { get; set; }
+
+        public void OnGet() {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync() {
+            if (!ModelState.IsValid) {
+                return Page();
+            }
+
+            return RedirectToPage("Index");
+        }
+    }
+
+    
+
+    public class ContactFormModel {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
