973408b8d9a97ab5ef9fc4f34d263bddef7b88c7
eempc <evc484@gmail.com>
2019-11-13 08:40:24 +0000

    finished as in web app 1 test

---
+++ WebApplication2/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication2.Areas.Identity.Data;
+
+namespace WebApplication2.Areas.Identity.Pages.Account.Manage {
+    public partial class IndexModel : PageModel {
+        private readonly UserManager<WebApplication2User> _userManager;
+        private readonly SignInManager<WebApplication2User> _signInManager;
+
+        public IndexModel(
+            UserManager<WebApplication2User> userManager,
+            SignInManager<WebApplication2User> signInManager) {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public string Username { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel {
+            // **************** Ask for full name and date of birth, same properties as the user model ****************
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "Full name")]
+            public string Name { get; set; }
+
+            [Required]
+            [DataType(DataType.Date)]
+            [Display(Name = "Birth date")]
+            public DateTime DateOfBirth { get; set; }
+
+            [Phone]
+            [Display(Name = "Phone number")]
+            public string PhoneNumber { get; set; }
+        }
+
+        private async Task LoadAsync(WebApplication2User user) {
+            var userName = await _userManager.GetUserNameAsync(user);
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+
+            Username = userName;
+
+            // **************** Update the input model here ****************
+            Input = new InputModel {
+                Name = user.Name,
+                DateOfBirth = user.DateOfBirth,
+                PhoneNumber = phoneNumber
+            };
+        }
+
+        public async Task<IActionResult> OnGetAsync() {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null) {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            await LoadAsync(user);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync() {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null) {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (!ModelState.IsValid) {
+                await LoadAsync(user);
+                return Page();
+            }
+
+            // **************** Add some checks in here for the new properties ****************
+            if (Input.Name != user.Name) user.Name = Input.Name;
+            if (Input.DateOfBirth != user.DateOfBirth) user.DateOfBirth = Input.DateOfBirth;
+
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            if (Input.PhoneNumber != phoneNumber) {
+                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+                if (!setPhoneResult.Succeeded) {
+                    var userId = await _userManager.GetUserIdAsync(user);
+                    throw new InvalidOperationException($"Unexpected error occurred setting phone number for user with ID '{userId}'.");
+                }
+            }
+
+            // **************** Then finally do this update thing ****************
+            await _userManager.UpdateAsync(user);
+
+            await _signInManager.RefreshSignInAsync(user);
+            StatusMessage = "Your profile has been updated";
+            return RedirectToPage();
+        }
+    }
+}
